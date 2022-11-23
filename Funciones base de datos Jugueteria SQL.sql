71f26fe7ca1a39e9d8f9839ef7c08357d84f6b18
Franco Carranza <109978872+FrancoCarranza@users.noreply.github.com>
2022-11-23 16:55:34 -0300

    Add files via upload

---
+++ Funciones base de datos Jugueteria SQL.sql
@@ -0,0 +1,123 @@
+/* Creacion de funciones en base de datos jugueteria */
+
+USE jugueteria;
+
+/* Creamos la primera funcion que servira para traer el stock total del juguete que le digamos,
+ le pasaremos como parametro el id del juguete */
+
+DELIMITER $$
+
+CREATE FUNCTION `juguete_stock` (juguete INT)
+RETURNS INT
+READS SQL DATA
+BEGIN
+	DECLARE stock_juguete INT;
+    SET stock_juguete=(SELECT stock from stock WHERE juguete=id_juguete);
+    RETURN stock_juguete;
+END $$
+
+DELIMITER ;
+
+/* Creamos la segunda funcion la cual nos dira la cantidad de juguetes que nos vendio un proveedor
+  pasandole como parametros el id del poveedor */
+  
+DELIMITER $$
+
+CREATE FUNCTION `cantidad_juguetes_proveedor` (proveedor INT)
+RETURNS INT
+READS SQL DATA
+BEGIN
+	DECLARE proveedor_juguetes INT;
+	SET proveedor_juguetes=(SELECT COUNT(nombre) FROM juguetes WHERE proveedor=id_proveedor);
+    RETURN proveedor_juguetes;
+END$$
+
+DELIMITER ;
+
+/* Creamos las proximas tres funciones las cuales nos dian la cantidad de stock total que hay en cada ubicacion,
+   sin la necesidad que le pasemos un parametro, solo seleccionandolas*/
+
+DELIMITER $$
+
+CREATE FUNCTION `deposito_stock` ()
+RETURNS INT
+READS SQL DATA
+BEGIN
+	DECLARE deposito_stock INT;
+    SET deposito_stock=(SELECT SUM(stock) FROM stock_deposito);
+    RETURN deposito_stock;
+END $$
+
+CREATE FUNCTION `sucursal_1_stock` ()
+RETURNS INT
+READS SQL DATA
+BEGIN
+	DECLARE sucursal_1_stock INT;
+    SET sucursal_1_stock=(SELECT SUM(stock) FROM stock_sucursal_1);
+    RETURN sucursal_1_stock;
+END $$
+
+CREATE FUNCTION `sucursal_2_stock` ()
+RETURNS INT
+READS SQL DATA
+BEGIN
+	DECLARE sucursal_2_stock INT;
+    SET sucursal_2_stock=(SELECT SUM(stock) FROM stock_sucursal_2);
+    RETURN sucursal_2_stock;
+END $$
+
+DELIMITER ;
+
+/* Agregamos la sexta funcion la cual servira para traer el precio de un juguete, como parameto le pasamos el id del juguete,
+   esta funcion decidi crearla para agilizar la insercion de datos en la tabla compras 
+   para no perder tiempo buscando el precio de cada juguete */
+
+DELIMITER $$
+
+CREATE FUNCTION `precio_juguete` (juguete INT)
+RETURNS DECIMAL
+READS SQL DATA
+BEGIN
+	DECLARE juguete_precio DECIMAL;
+    SET juguete_precio=(SELECT precio_juguete FROM juguetes WHERE id_juguete=juguete);
+    RETURN juguete_precio;
+END $$
+
+DELIMITER ;
+
+/* Las proximas tes funciones serviran para sacar los diferentes tipos de descuentos que ofrece la jugueteria,
+con tajeta Club la Voz hay un %15 de descuento, con tajeta Naranja un %20 y pagando en efectivo un %10,
+estas funciones son utilizadas en el tigger precios_juguetes para llenar la tabla precios_juguetes. */
+
+DELIMITER $$
+
+CREATE FUNCTION `descuento_naranja` (precio DECIMAL)
+RETURNS DECIMAL
+READS SQL DATA
+BEGIN
+	DECLARE precio_naranja DECIMAL;
+    SET precio_naranja=(precio - (precio*20)/100);
+    RETURN precio_naranja;
+END $$
+
+
+CREATE FUNCTION `descuento_clubLaVoz` (precio DECIMAL)
+RETURNS DECIMAL
+READS SQL DATA
+BEGIN
+	DECLARE precio_clubLaVoz DECIMAL;
+    SET precio_clubLaVoz=(precio - (precio*15)/100);
+    RETURN precio_clubLaVoz;
+END $$
+
+
+CREATE FUNCTION `descuento_efectivo` (precio DECIMAL)
+RETURNS DECIMAL
+READS SQL DATA
+BEGIN
+	DECLARE precio_efectivo DECIMAL;
+    SET precio_efectivo=(precio - (precio*10)/100);
+    RETURN precio_efectivo;
+END $$
+
+DELIMITER ; 
